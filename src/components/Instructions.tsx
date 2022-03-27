36c86f53d3cfe87af63329b3eb0ef4aff2c5f47f
Zachary Lin <zcl217@nyu.edu>
2022-03-27 11:32:04 +0900

    Fix: Instructions styling

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -7,8 +7,6 @@ interface InstructionsProps {
     controlKeyText?: string[],
     isLargeScreen?: boolean
 }
-// TODO: the styling for this component doesn't really work
-// for small screens right now. fix.
 const Instructions: React.FC<InstructionsProps> = (props) => {
     const { mainText, controlKeys, controlKeyText, isLargeScreen } = props;
 
@@ -17,7 +15,7 @@ const Instructions: React.FC<InstructionsProps> = (props) => {
         const controlList = [];
         for (let a = 0; a < controlKeys.length; a++) {
             if (controlKeyText) {
-                controlList.push(<div className="flex flex-row" key={controlKeys[a]}>
+                controlList.push(<div className="flex flex-row items-center" key={controlKeys[a]}>
                     <span className="flex items-center justify-center p-2 pb-3 mx-2 text-black bg-white border-2 border-black rounded-md control-key"> {controlKeys[a]} </span>
                     <span className=""> {controlKeyText[a]} {a < controlKeys.length - 1 && '|'} </span>
                 </div>);
@@ -36,9 +34,10 @@ const Instructions: React.FC<InstructionsProps> = (props) => {
                 className="z-30 flex flex-col items-center justify-around w-full overflow-auto bg-white shadow-xl"
                 animate={INSTRUCTIONS_ANIMATION}
             >
-                <h1 className={`w-4/5 text-center ${isLargeScreen ? 'text-[3vw]' : 'text-[2vw]'}`}> {mainText} </h1>
-                <div className="flex flex-col items-center w-full">
-                    <div className="flex font-size-[2vw] test">
+                {/* <h1 className={`w-4/5 text-center ${isLargeScreen ? 'text-[3vw]' : 'text-[2vw]'}`}> {mainText} </h1> */}
+                <h1 className='w-4/5 text-center min-font-size'> {mainText} </h1>
+                <div className="flex flex-col items-center w-full overflow-y-hidden">
+                    <div className="flex min-font-size">
                         {controlList()}
                     </div>
                 </div>
